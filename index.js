1445b1e5dd8fa26a6cb282ad8cb6a210777ad4a2
Aaron Chu <aaronchu357@gmail.com>
2019-06-16 10:34:08 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.slice(0, 1).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-'\s]+/g, '')
+  }
+
+  static titleize(string) {
+    this._lower = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let springCleanedString = this.sanitize(string)
+    let springCleanedWords = springCleanedString.split(" ")
+    let springTitleizedWords = springCleanedWords.map(word => {
+      return !this._lower.includes(word) || springCleanedWords.indexOf(word) === 0? this.capitalize(word):word
+    })
+    return springTitleizedWords.join(" ")
+  }
 }
